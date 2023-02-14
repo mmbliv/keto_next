976162ae9d12a90c9449fbd9d0a8083d747f8647
min <mmbliv@gmail.com>
2023-02-14 16:21:17 -0600

    add env

--- src/utils/fetch.tsx
+++ src/utils/fetch.tsx
@@ -3,7 +3,7 @@ import { CardType } from "@/types/type";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "3a7cfed0bbmsh8015a4bc6f08d36p1f95d0jsn7fc4d9efe6db",
+    "X-RapidAPI-Key": process.env.API_KEY as string,
     "X-RapidAPI-Host": "keto-diet.p.rapidapi.com",
   },
 };
